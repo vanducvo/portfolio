b3f8254ec47c77268f672a55befc9947af15a585
Van Duc Vo <vanducvo@gmail.com>
2020-05-01 19:50:56 +0700

    navigation bar

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Navigation from '../navigation/navigation';
+
+function App(props){
+    return(
+        <Fragment>
+            <Navigation />
+        </Fragment>
+    );
+}
+
+export default App;
